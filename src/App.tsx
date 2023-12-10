2aa841cca115692ac288962aa579c3f593376295
edsonarios <edsonrios9@gmail.com>
2023-12-09 21:14:50 -0400

    Refactor with new feature, split panel in aside and main section, observable

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { type StoreType, usePlayerStore } from './store/playerStore'
 import { PlaylistPipMode } from './components/body/pipMode/Playlist'
 import { PlaylistDetail } from './components/body/pipMode/PlaylistDetail'
 import { useEffect, useState } from 'react'
+import Split from 'react-split'
+
 interface fileWithMedata {
   id: string
   name: string
@@ -20,7 +22,10 @@ interface OpenDirectoryDialog {
 
 interface ElectronAPI {
   openDirectoryDialog: () => Promise<OpenDirectoryDialog>
-  receive: (channel: string, func: (event: any, ...args: any[]) => void) => void
+  receive: (
+    channel: string,
+    func: (event: any, ...args: any[]) => void
+  ) => void
   removeListener: (channel: string, func: (...args: any[]) => void) => void
 }
 
@@ -31,13 +36,16 @@ declare global {
 }
 
 export default function App () {
-  const { currentMusic, pictureInPicture, playlistView, playlists } = usePlayerStore<StoreType>(state => state)
+  const { currentMusic, pictureInPicture, playlistView, playlists } =
+    usePlayerStore<StoreType>((state) => state)
 
   const [currentColor, setCurrentColor] = useState(colors.gray.dark)
   useEffect(() => {
     let newColor = colors.gray.dark
     if (playlistView !== '0' && pictureInPicture) {
-      const viewPlaylist = playlists.find(playlist => playlist.id === playlistView)
+      const viewPlaylist = playlists.find(
+        (playlist) => playlist.id === playlistView
+      )
       if (viewPlaylist !== undefined) {
         newColor = viewPlaylist.color.dark
       }
@@ -55,33 +63,42 @@ export default function App () {
   }
 
   return (
-    <div id='app' className='relative h-screen p-2 gap-3'>
-      <aside className='[grid-area:aside] flex-col flex overflow-y-auto'>
-        <AsideMenu />
-      </aside>
+    <div id="app" className="h-screen p-2 gap-3">
+      <div className="[grid-area:main] overflow-y-auto">
+        <Split
+          className="flex flex-row h-full"
+          sizes={[15, 85]}
+          minSize={[300, 900]}
+          gutterSize={10}
+          cursor="col-resize"
+        >
+          <aside className="flex flex-col overflow-y-auto">
+            <AsideMenu />
+          </aside>
 
-      <main
-        className='[grid-area:main] rounded-lg overflow-y-auto w-full h-full flex justify-center items-center flex-col relative'
-        style={{
-          background: `linear-gradient(to bottom, ${currentColor}, #18181b)`,
-          transition: 'opacity 0.5s ease'
-        }}
-      >
-        {/* Just gradient with pictureInPicture is enable */}
-        <div
-          className="absolute top-0 left-0 w-full h-full"
-          style={{
-            background: 'linear-gradient(to bottom, #2c2c2c, #18181b)',
-            transition: 'opacity 0.7s ease',
-            opacity: pictureInPicture && playlistView === '0' ? 1 : 0
-          }}
-        />
-        <Header />
-        <PlayerComponent />
-        {pictureInPicture && setPlaylist()}
-      </main>
-
-      <footer className='[grid-area:player] h-[80px]'>
+          <main
+            className="rounded-lg overflow-y-auto w-full h-full flex justify-center items-center relative flex-col"
+            style={{
+              background: `linear-gradient(to bottom, ${currentColor}, #18181b)`,
+              transition: 'opacity 0.5s ease'
+            }}
+          >
+            {/* Just gradient with pictureInPicture is enable */}
+            <div
+              className="absolute top-0 left-0 w-full h-full"
+              style={{
+                background: 'linear-gradient(to bottom, #2c2c2c, #18181b)',
+                transition: 'opacity 0.7s ease',
+                opacity: pictureInPicture && playlistView === '0' ? 1 : 0
+              }}
+            />
+            <Header />
+            <PlayerComponent />
+            {pictureInPicture && setPlaylist()}
+          </main>
+        </Split>
+      </div>
+      <footer className="[grid-area:player] flex flex-row h-[80px]">
         <Controls />
       </footer>
     </div>
