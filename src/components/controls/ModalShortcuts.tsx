80976ae8fd148fc1498b5625abc871cdb3e8b4ac
edsonarios <edsonrios9@gmail.com>
2024-01-28 18:16:41 -0400

    Added translates to shortcuts

--- src/components/controls/ModalShortcuts.tsx
+++ src/components/controls/ModalShortcuts.tsx
@@ -43,9 +43,9 @@ export default function ModalShowShortcuts () {
               <table className="text-left w-full ">
                 <tbody>
                   <tr>
-                    <td className="px-4 py-2 ">Play/Pause :</td>
+                    <td className="px-4 py-2 ">{t('controls.playPause')} :</td>
                     <td className="px-4 py-2 flex flex-row justify-center items-center">
-                      Global Media
+                      {t('controls.media')}
                       <div className="border-2 rounded-md border-zinc-600 py-1 px-2 flex flex-row items-center ml-4">
                         <PlayPauseIcon />
                       </div>
@@ -55,30 +55,30 @@ export default function ModalShowShortcuts () {
                     <td className="px-4 py-1 "></td>
                     <td className="px-4 py-1 flex flex-row justify-center items-center mb-2">
                       <div className="border-2 rounded-md border-zinc-600 py-1 px-2 flex flex-row items-center ml-4">
-                        Space
+                        {t('controls.space')}
                       </div>
                     </td>
                   </tr>
                   <tr>
-                    <td className="px-4 py-2 ">Next :</td>
+                    <td className="px-4 py-2 ">{t('controls.next')} :</td>
                     <td className="px-4 py-2 flex flex-row justify-center items-center mb-2">
-                      Global Media
+                      {t('controls.media')}
                       <div className="border-2 rounded-md border-zinc-600 py-1 px-2 flex flex-row items-center ml-4">
                         <NextIcon />
                       </div>
                     </td>
                   </tr>
                   <tr>
-                    <td className="px-4 py-2 ">Previous :</td>
+                    <td className="px-4 py-2 ">{t('controls.previous')} :</td>
                     <td className="px-4 py-2 flex flex-row justify-center items-center mb-2">
-                      Global Media
+                      {t('controls.media')}
                       <div className="border-2 rounded-md border-zinc-600 py-1 px-2 flex flex-row items-center ml-4">
                         <PreviousIcon />
                       </div>
                     </td>
                   </tr>
                   <tr>
-                    <td className="px-4 py-2 ">Picture in picture mode :</td>
+                    <td className="px-4 py-2 ">{t('controls.pip')} :</td>
                     <td className="px-4 py-2 flex flex-row justify-center items-center mb-2">
                       <div className="border-2 rounded-md border-zinc-600 px-2 flex flex-row items-center ml-4">
                         P
@@ -86,7 +86,7 @@ export default function ModalShowShortcuts () {
                     </td>
                   </tr>
                   <tr>
-                    <td className="px-4 py-2 ">Mute :</td>
+                    <td className="px-4 py-2 ">{t('controls.mute')} :</td>
                     <td className="px-4 py-2 flex flex-row justify-center items-center mb-2">
                       <div className="border-2 rounded-md border-zinc-600 py-1 px-2 flex flex-row items-center ml-4">
                         M
@@ -94,7 +94,7 @@ export default function ModalShowShortcuts () {
                     </td>
                   </tr>
                   <tr>
-                    <td className="px-4 py-2 ">Seek Forward/Backward :</td>
+                    <td className="px-4 py-2 ">{t('controls.seekFB')} :</td>
                     <td className="px-4 py-2 flex flex-row justify-center items-center mb-2">
                       <div className="border-2 rounded-md border-zinc-600 py-1 px-2 flex flex-row items-center ml-4">
                         {'→'}
@@ -105,7 +105,7 @@ export default function ModalShowShortcuts () {
                     </td>
                   </tr>
                   <tr>
-                    <td className="px-4 py-2 ">Seek to 10% - 90% :</td>
+                    <td className="px-4 py-2 ">{t('controls.seek%')} :</td>
                     <td className="px-4 py-2 flex flex-row justify-center items-center mb-2">
                       <div className="border-2 rounded-md border-zinc-600 py-1 px-2 flex flex-row items-center ml-4">
                         1 - 9
@@ -113,7 +113,7 @@ export default function ModalShowShortcuts () {
                     </td>
                   </tr>
                   <tr>
-                    <td className="px-4 py-2 ">Volume Up/Down :</td>
+                    <td className="px-4 py-2 ">{t('controls.volumeUpDown')} :</td>
                     <td className="px-4 py-2 flex flex-row justify-center items-center mb-2">
                       <div className="border-2 rounded-md border-zinc-600 py-1 px-2 flex flex-row items-center ml-4">
                         {'↑'}
@@ -124,7 +124,7 @@ export default function ModalShowShortcuts () {
                     </td>
                   </tr>
                   <tr>
-                    <td className="px-4 py-2 ">Speed increase/decrease :</td>
+                    <td className="px-4 py-2 ">{t('controls.sppedID')} :</td>
                     <td className="px-4 py-2 flex flex-row justify-center items-center mb-2">
                       <div className="border-2 rounded-md border-zinc-600 py-1 px-2 flex flex-row items-center ml-4">
                         +
@@ -135,7 +135,7 @@ export default function ModalShowShortcuts () {
                     </td>
                   </tr>
                   <tr>
-                    <td className="px-4 py-2 ">Full Screen :</td>
+                    <td className="px-4 py-2 ">{t('controls.fullScreen')} :</td>
                     <td className="px-4 py-2 flex flex-row justify-center items-center mb-2">
                       <div className="border-2 rounded-md border-zinc-600 py-1 px-2 flex flex-row items-center ml-4">
                         F
@@ -143,7 +143,7 @@ export default function ModalShowShortcuts () {
                     </td>
                   </tr>
                   <tr>
-                    <td className="px-4 py-2 ">Open Folder :</td>
+                    <td className="px-4 py-2 ">{t('aside.folder')} :</td>
                     <td className="px-4 py-2 flex flex-row justify-center items-center mb-2">
                       <div className="border-2 rounded-md border-zinc-600 py-1 px-2 flex flex-row items-center ml-4">
                         O
